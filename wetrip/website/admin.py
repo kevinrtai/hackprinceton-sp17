47b1540e7f038f92d1d0e3b39143d45abdd6d5fc
Kevin Tai <kevin.r.tai@gmail.com>
2017-04-01 11:26:30 -0400

    Models

--- wetrip/website/admin.py
+++ wetrip/website/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Login, User, Group, Media, Review, Destination, Trip
+
 # Register your models here.
+admin.site.register(Login)
+admin.site.register(User)
+admin.site.register(Group)
+admin.site.register(Media)
+admin.site.register(Review)
+admin.site.register(Destination)
+admin.site.register(Trip)
+
--- wetrip/website/models.py
+++ wetrip/website/models.py
@@ -7,45 +7,64 @@ from django.db import models
 class Login(models.Model):
   username = models.CharField(max_length=20)
   password = models.CharField(max_length=40)
+  
+  def __str__(self):
+    return self.username
 
 
 class User(models.Model):
   login = models.OneToOneField(Login, on_delete=models.CASCADE, primary_key=True)
-  email = models.EmailField(max_length=20)
-  name = models.CharField(max_length=20)
-  profile_pic = models.URLField()
-  groups = models.ManyToManyField('Group')
-  friends = models.ForeignKey('self', on_delete=models.CASCADE)
+  email = models.EmailField(max_length=50)
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
+  profile_pic = models.URLField(blank=True, null=True)
+  groups = models.ManyToManyField('Group', blank=True, null=True)
+  friends = models.ManyToManyField('self', blank=True, null=True)
+
+  def __str__(self):
+    return self.first_name + self.last_name
 
 
 class Group(models.Model):
   group_name = models.CharField(max_length=20)
 
+  def __str__(self):
+    return self.group_name
+
 
 class Media(models.Model):
+  media_title = models.CharField(max_length=20)
   media_url = models.URLField()
   uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.media_url
 
 
 class Review(models.Model):
   review_text = models.TextField()
   author = models.ForeignKey(User, on_delete=models.CASCADE)
-  media = models.ManyToManyField(Media)
+  media = models.ManyToManyField(Media, blank=True, null=True)
   destination = models.ForeignKey('Destination')
+
+  def __str__(self):
+    return str(self.author) + str(self.id)
   
 
 class Destination(models.Model):
   name = models.CharField(max_length=20)
-  parent = models.ForeignKey('self', on_delete=models.CASCADE)
-  nation = models.BooleanField()
-  city = models.BooleanField()
-  attraction = models.BooleanField()
-  trips = models.ForeignKey
+  parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
 
 
 class Trip(models.Model):
   participants = models.ManyToManyField(User)
   name = models.CharField(max_length=20)
   owner = models.ForeignKey(User, related_name="owner")
-  dests = models.ManyToManyField(Destination)
+  dests = models.ManyToManyField(Destination, blank=True, null=True)
+
+  def __str__(self):
+    return self.name
 
