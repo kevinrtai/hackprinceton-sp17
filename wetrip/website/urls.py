b5e2eda6410ca2efea6d07b4701670f6ab596039
Kevin Tai <kevin.r.tai@gmail.com>
2017-04-01 20:37:17 -0400

    wired some of the views together

--- wetrip/website/urls.py
+++ wetrip/website/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', auth_views.login, {'template_name': 'website/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'website/logout.html'}, name='logout'), 
-    url(r'^profile', views.profile, name='profile'),
-    url(r'^destination', views.destination, name='destination'),
-    url(r'^tripplanner', views.planning, name='tripplanner'),
-    url(r'^search', views.search , name='search')
+    url(r'^profile/(?P<username>[a-zA-Z]+)/(?P<page>[a-zA-Z]+)/', views.profile, name='profile'),
+    url(r'^destination/(?P<dest_id>[0-9]+)/(?P<page>[a-zA-z]+)/', views.destination, name='destination'),
+    url(r'^tripplanner/(?P<trip_id>[0-9]+)/', views.planning, name='tripplanner'),
+    url(r'^search/', views.search , name='search')
 ]
--- wetrip/website/views.py
+++ wetrip/website/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+from website.models import Login, User, Group, Media, Review, Destination, DestinationInfo, Trip
 
 # Create your views here.
 def index(request):
@@ -9,13 +11,47 @@ def index(request):
 def login(request):
   return render(request, 'website/login.html')
 
-def profile(request):
-  return render(request, 'website/profile-about.html')
+def profile(request, username, page):
+  # 404 if username not in database
+  if False:
+    return HttpResponseNotFound('<h1>Page not found</h1>')
 
-def destination(request):
-  return render(request, 'website/destination-reviews.html')
+  # construct context
+  context = {'username': username}
 
-def planning(request):
+  # Return the subpage that has been requested 
+  if page == 'about':
+    return render(request, 'website/profile-about.html', context)    
+  elif page == 'bookmarks':
+    return render(request, 'website/profile-bookmarks.html', context)
+  elif page == 'photos':
+    return render(request, 'website/profile-photos.html', context)
+  elif page == 'reviews':
+    return render(request, 'website/profile-reviews.html', context)
+
+  # return 404 
+  return HttpResponseNotFound('<h1>Page not found</h1>')
+
+def destination(request, dest_id, page):
+  # 404 if dest_id not in database
+  if False:
+    return HttpResponseNotFound('<h1>Page not found</h1>')
+
+  # construct context
+  if page == 'reviews':
+    return render(request, 'website/destination-reviews.html')
+  elif page == 'bookmarks':
+    return render(request, 'website/destination-bookmark.html')
+  elif page == 'friends':
+    return render(request, 'website/destination-friends.html')
+  elif page == 'photos':
+    return render(request, 'website/destination-photos.html')
+  elif page == 'creategroup':
+    return render(request, 'website/destination-creategroup.html')
+    
+  return HttpResponseNotFound('<h1>Page not found</h1>') 
+
+def planning(request, trip_id):
   return render(request, 'website/planning.html')
 
 def search(request):
